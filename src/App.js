decb6868a7358d1206f1f65b4e055431bc2ae727
DagimDeresse <Dagimyeshewalul@gmail.com>
2023-11-20 19:46:39 -0500

    Signup fixed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-        <Route path="/" exact Component={StoreSelector}/>
+        <Route path="/" exact Component={SignUp}/>
           {/* <Route path="/Home" element={<Home />} /> */}
         </Routes>
       </Router>
