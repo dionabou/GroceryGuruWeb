8423748574d70fb6b5a2b3fe661deca4ac3b05a0
DagimDeresse <Dagimyeshewalul@gmail.com>
2023-12-11 19:20:01 -0500

    Categories done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import StoreSelector from './pages/StoreSelector';
+import { BuildingList } from './pages/BuildingList';
+import Home from './pages/Home';
+import Login from './pages/Login';
+// import Logout from './pages/Logout';
+import { ProductDetails } from './pages/ProductDetails';
+import SignUp from './pages/SignUp';
+import UpdatePassword from './pages/UpdatePassword';
+import { Main } from './ShoppingList/Main';
+import { Compare } from './compare/Compare';
 import Categories from './pages/Categories';
 import About from './pages/About';
+import StoreSelector from './pages/StoreSelector';
 
 function App() {
   return (
@@ -12,14 +21,18 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<StoreSelector />} />
+          <Route path="/" element={<SignUp />} />
+          <Route path="/Storeselector" element={<StoreSelector />} />
           <Route path="/categories/:addressId" element={<Categories />} />
-          <Route path="/About" element={<About />} />
+          <Route path="/ProductDetails/:productId" element={<ProductDetails />} />
+          <Route path="/BuildingList" element={<BuildingList />} />
+          
+          <Route path="/UpdatePassword" element={<UpdatePassword />} />
+          <Route path="/ShoppingList" element={<Main />} />
+          <Route path="/compare" element={<Compare />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-      <label htmlFor=""></label>
-      <label htmlFor=""></label>
-      <label htmlFor=""></label>
       <Footer />
     </div>
   );
--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import '../styles/Categories.css';
-function Categories() {
+
+const Categories = () => {
   const { addressId } = useParams();
 
   const [categoryData, setCategoryData] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [selectedCheckboxes, setSelectedCheckboxes] = useState({
-    
-  });
+  const [selectedSubcategory, setSelectedSubcategory] = useState(null);
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -43,25 +43,82 @@ function Categories() {
     fetchData();
   }, [addressId]);
 
-  const categorizeProduct = (category) => {
-    if (category.includes('milk') || category.includes('egg')) {
-      return 'Dairy';
-    } else if (category.includes('beef') || category.includes('chicken')) {
-      return 'Meat';
-    } else if (category.includes('toilet paper') || category.includes('paper towel')) {
-      return 'Paper Products';
-    } else if (category.includes('potato') || category.includes('onion') || category.includes('lettuce') || category.includes('tomato')) {
-      return 'Vegetables';
-    } else if (category.includes('apple') || category.includes('banana')) {
-      return 'Fruit';
+  // Reset state when changing the selected category
+  useEffect(() => {
+    setSelectedCategory(null);
+    setSelectedSubcategory(null);
+    setSelectedCheckboxes({});
+  }, [addressId]);
+
+  const organizedCategories = {
+    Dairy: {
+      Milk: ['milk', '1%', '2%', 'skim', 'whole', 'organic', 'gal', 'quart', 'qt', 'lactose free'],
+      Eggs: ['eggs', 'grade a', 'white', 'brown', 'large', 'extra large', 'cage free', 'jumbo', 'medium', 'free range', 'organic eggs'],
+    },
+    Meat: {
+      Beef: ['beef', 'ground', 'chuck', 'lean', 'angus', 'roast', 'hamburger', 'burger', 'goetta', 'shaved', 'steak', 'cubed', 'new york strip', 'sirloin', 'top round', 'ribeye', 'bottom round', 'flank', 'filet mignon', 'tenderloin','grass fed'],
+      Chicken: ['chicken', 'boneless', 'bone-in', 'wing', 'drumstick', 'breast', 'skinless', 'skin-on', 'tenderloin', 'thigh', 'organic', 'tender', 'whole', 'strip', 'gizzard', 'heart', 'leg'],
+    },
+    'Paper Products':{
+      'Toilet paper': ['toilet paper', 'regular roll', 'double roll', 'mega roll', '1 ply', '2 ply', '3 ply', 'scented', 'recycled', 'rv', 'marine'],
+      'Paper towel' : ['paper towels', 'double roll', 'double plus roll', 'giant roll', 'big roll', 'select-a-size', 'pick-a-size', 'tear-a-square'],
+  },
+    Fruit: { 
+      Apples: ['apples', 'honeycrisp', 'gala' , 'pink lady', 'granny smith', 'organic', 'fuji', 'sweettango', 'cosmic crisp', 'opal', 'envy', 'red delicious', 'golden delicious', 'ambrosia', 'cortland', ],
+    Bananas: [ 'bananas', 'organic', 'plantain'],
+  },
+    Vegetables:{
+      Potatoes: ['potatoes', 'russet', 'jumbo', 'petite', 'gol', 'yukon gold', 'idaho', 'red', 'sweet', 'organic', 'fingerling', 'gourmet',  ],
+    Onions: ['onions', 'sweet', 'jumbo', 'yellow', 'medium', 'white', 'red', 'green', 'organic', 'peruvian', 'gold', 'peeled', 'diced'],
+    Lettuce: ['lettuce', 'iceberg', 'shredded', 'romaine', 'heart', 'green', 'leaf', 'organic', 'living', 'butter', 'red', 'baby', 'boston'],
+    Tomatoes: ['tomatoes', 'red', 'on the vine', 'petite', 'medley', 'snacking', 'roma', 'large', 'beefsteak', 'grape', 'cherry', 'salad', 'campari', 'vine ripe', 'organic', 'heirloom', 'orange'],
+ 
     }
-
-    return null; // Returning null for products that don't fit into any category
   };
 
-  const handleCategoryClick = (category) => {
-    setSelectedCategory(category);
+  const categoryTags = {
+    Milk: [ '1%', '2%', 'skim', 'whole', 'organic', 'gal', 'quart', 'qt', 'lactose free'],
+    Eggs: [ 'grade a', 'white', 'brown', 'large', 'extra large', 'cage free', 'jumbo', 'medium', 'free range', 'organic eggs'],
+    Beef: [ 'ground', 'chuck', 'lean', 'angus', 'roast', 'hamburger', 'burger', 'goetta', 'shaved', 'steak', 'cubed', 'new york strip', 'sirloin', 'top round', 'ribeye', 'bottom round', 'flank', 'filet mignon', 'tenderloin','grass fed'],
+    Chicken: [ 'boneless', 'bone-in', 'wing', 'drumstick', 'breast', 'skinless', 'skin-on', 'tenderloin', 'thigh', 'organic', 'tender', 'whole', 'strip', 'gizzard', 'heart', 'leg'],
+    'Toilet paper': [ 'regular roll', 'double roll', 'mega roll', '1 ply', '2 ply', '3 ply', 'scented', 'recycled', 'rv', 'marine'],
+    'Paper towel' : [ 'double roll', 'double plus roll', 'giant roll', 'big roll', 'select-a-size', 'pick-a-size', 'tear-a-square'],
+    Apples: [ 'honeycrisp', 'gala' , 'pink lady', 'granny smith', 'organic', 'fuji', 'sweettango', 'cosmic crisp', 'opal', 'envy', 'red delicious', 'golden delicious', 'ambrosia', 'cortland', ],
+    Bananas: [  'organic', 'plantain'],
+    Potatoes: [ 'russet', 'jumbo', 'petite', 'gol', 'yukon gold', 'idaho', 'red', 'sweet', 'organic', 'fingerling', 'gourmet',  ],
+    Onions: [ 'sweet', 'jumbo', 'yellow', 'medium', 'white', 'red', 'green', 'organic', 'peruvian', 'gold', 'peeled', 'diced'],
+    Lettuce: [ 'iceberg', 'shredded', 'romaine', 'heart', 'green', 'leaf', 'organic', 'living', 'butter', 'red', 'baby', 'boston'],
+    Tomatoes: [ 'red', 'on the vine', 'petite', 'medley', 'snacking', 'roma', 'large', 'beefsteak', 'grape', 'cherry', 'salad', 'campari', 'vine ripe', 'organic', 'heirloom', 'orange'],
   };
+
+
+  const handleCategoryClick = (category, event) => {
+    event.preventDefault();
+    if (selectedCategory === category) {
+      // If the clicked category is already open, close it
+      setSelectedCategory(null);
+      setSelectedSubcategory(null);
+      setSelectedCheckboxes({});
+    } else {
+      // If the clicked category is closed, open it
+      setSelectedCategory(category);
+      setSelectedSubcategory(null);
+      setSelectedCheckboxes({});
+    }
+  };
+  
+  const handleSubcategoryClick = (subcategory) => {
+    if (selectedSubcategory === subcategory) {
+      // If the clicked subcategory is already open, close it
+      setSelectedSubcategory(null);
+      setSelectedCheckboxes({});
+    } else {
+      // If the clicked subcategory is closed, open it
+      setSelectedSubcategory(subcategory);
+      setSelectedCheckboxes({});
+    }
+  };
+  
 
   const handleCheckboxChange = (checkbox) => {
     setSelectedCheckboxes((prevCheckboxes) => ({ ...prevCheckboxes, [checkbox]: !prevCheckboxes[checkbox] }));
@@ -75,74 +132,71 @@ function Categories() {
     return <p>Error: {error}</p>;
   }
 
-  const organizedCategories = {
-    Dairy: ['milk', 'eggs'],
-    Meat: ['beef', 'chicken'],
-    'Paper Products': ['toilet paper', 'paper towels'],
-    Vegetables: ['potatoes', 'onions', 'lettuce', 'tomatoes'],
-    Fruit: ['apples', 'bananas'],
-  };
-
-  const categoryTags = {
-    Dairy: ['milk', '1%', '2%', 'skim', 'whole', 'organic', 'gal', 'quart', 'qt', 'lactose free', 'eggs', 'grade a', 'white', 'brown', 'large', 'extra large', 'cage free', 'jumbo', 'medium', 'free range', 'organic eggs'],
-    Meat: ['beef', 'ground', 'chuck', 'lean', 'angus', 'roast', 'hamburger', 'burger', 'goetta', 'shaved', 'steak', 'cubed', 'new york strip', 'sirloin', 'top round', 'ribeye', ' bottom round', 'flank', 'filet mignon', 'tenderloin','grass fed', 'chicken', 'boneless', 'bone-in', 'wing', 'drumstick', 'breast', 'skinless', 'skin-on', 'tenderloin', 'thigh', 'organic', 'tender', 'whole', 'strip', 'gizzard', 'heart', 'leg'],
-    'Paper Products': ['toilet paper', 'regular roll', 'double roll', 'mega roll', '1 ply', '2 ply', '3 ply', 'scented', 'recycled', 'rv', 'marine', 'paper towels', 'double roll', 'double plus roll', 'giant roll', 'big roll', 'select-a-size', 'pick-a-size', 'tear-a-square'],
-    Fruit: ['apples', 'honeycrisp', 'gala' , 'pink lady', 'granny smith', 'organic', 'fuji', 'sweettango', 'cosmic crisp', 'opal', 'envy', 'red delicious', 'golden delicious', 'ambrosia', 'cortland',   'bananas', 'organic', 'plantain'],
-    Vegetables: ['potatoes', 'russet', 'jumbo', 'petite', 'gol', 'yukon gold', 'idaho', 'red', 'sweet', 'organic', 'fingerling', 'gourmet', 'onions', 'sweet', 'jumbo', 'yellow', 'medium', 'white', 'red', 'green', 'organic', 'peruvian', 'gold', 'peeled', 'diced', 'lettuce', 'iceberg', 'shredded', 'romaine', 'heart', 'green', 'leaf', 'organic', 'living', 'butter', 'red', 'baby', 'boston', 'tomatoes', 'red', 'on the vine', 'petite', 'medley', 'snacking', 'roma', 'large', 'beefsteak', 'grape', 'cherry', 'salad', 'campari', 'vine ripe', 'organic', 'heirloom', 'orange'],
-  };
-
   return (
     <div className="container">
       <h2>Categories for Address ID: {addressId}</h2>
       <ul>
-        {Object.entries(organizedCategories).map(([category, products]) => (
+        {Object.entries(organizedCategories).map(([category, subcategories]) => (
           <li key={category}>
-            <h3 onClick={() => handleCategoryClick(category)}>{category}</h3>
+            <h3 onClick={(event) => handleCategoryClick(category, event)}>{category}</h3>
             {selectedCategory === category && (
-              <div>
-                {/* Filter buttons for the selected category */}
-                {categoryTags[category].map(tag => (
-                  <button
-                    key={tag}
-                    onClick={() => handleCheckboxChange(tag)}
-                    className={selectedCheckboxes[tag] ? 'active' : ''}
-                  >
-                    {tag}
-                  </button>
-                ))}
-                {/* Product list */}
-                <ul>
-                  {categoryData
-                    .filter((cat) => products.includes(cat.category.toLowerCase()))
-                    .flatMap((cat) =>
-                      cat.productList
-                        .filter((product) => {
-                          // Check if any selected checkbox matches the product's name or description
-                          return Object.entries(selectedCheckboxes).some(
-                            ([checkbox, isChecked]) =>
-                              isChecked &&
-                              (product.name.toLowerCase().includes(checkbox) ||
-                                product.description.toLowerCase().includes(checkbox))
-                          );
-                        })
-                        .map((product) => (
-                          <li key={product.id}>
-                            <div>
-                              <p>Name: {product.name}</p>
-                              <p>Price: {product.price}</p>
-                              <img src={product.imageUrl} alt={product.name} />
-                            </div>
-                          </li>
-                        ))
+              <ul>
+                {Object.keys(subcategories).map((subcategory) => (
+                  <li key={subcategory}>
+                    <h4 onClick={() => handleSubcategoryClick(subcategory)}>{subcategory}</h4>
+                    {selectedSubcategory === subcategory && (
+                      <div>
+                        {/* Filter buttons for the selected subcategory */}
+                        {categoryTags[subcategory].map(tag => (
+                          <button
+                            key={tag}
+                            onClick={() => handleCheckboxChange(tag)}
+                            className={selectedCheckboxes[tag] ? 'active' : ''}
+                          >
+                            {tag}
+                          </button>
+                        ))}
+                        {/* Product list with links to product details */}
+                        <ul>
+                          {categoryData
+                            .filter((cat) => subcategories[subcategory].includes(cat.category?.toLowerCase()))
+                            .flatMap((cat) =>
+                              cat.productList
+                                .filter((product) =>
+                                  Object.entries(selectedCheckboxes).every(
+                                    ([checkbox, isChecked]) =>
+                                      !isChecked ||
+                                      (
+                                        (product.name && product.name.toLowerCase().includes(checkbox)) ||
+                                        (product.description && product.description.toLowerCase().includes(checkbox))
+                                      )
+                                  )
+                                )
+                                .map((product) => (
+                                  <li key={product.id}>
+                                    {/* Use Link to navigate to product details page with productId */}
+                                    <Link to={`/product-details/${product.id}`}>
+                                      <div>
+                                        <p>Name: {product.name}</p>
+                                        <p>Price: {product.price}</p>
+                                        <img src={product.imageUrl} alt={product.name} />
+                                      </div>
+                                    </Link>
+                                  </li>
+                                ))
+                            )}
+                        </ul>
+                      </div>
                     )}
-                </ul>
-              </div>
+                  </li>
+                ))}
+              </ul>
             )}
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default Categories;
